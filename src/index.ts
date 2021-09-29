318e294761ed639ccede48f5b9275dc33fc41e61
Amy Pellegrini <amyvpellegrini@gmail.com>
2021-09-29 17:43:26 +0100

    Add missing import/exports for new components from main package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as Accidental } from './accidental';
 export { default as Staff } from './staff';
 export { default as Clef } from './clef';
+export { default as Stave } from './stave';
+export { default as KeySignature } from './key-signature';
